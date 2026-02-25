fa2d7433ddcb08841e0c321817a4a92a88f6c30f
dmccowa206 <mccowa206@morrisville.edu>
2026-02-25 12:25:59 -0500

    Converted UI Button to XR Button Interactable

---
+++ Assets/Scripts/XRButtonInteractable.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.UI;
+
+public class XRButtonInteractable : XRSimpleInteractable
+{
+    [SerializeField] Image buttonImage;
+    [SerializeField] Color[] buttonColors = new Color[4];
+    private Color normalColor, highlightedColor, pressedColor, selectedColor;
+    private bool isPressed;
+    void Start()
+    {
+        normalColor = buttonColors[0];
+        highlightedColor = buttonColors[1];
+        pressedColor = buttonColors[2];
+        selectedColor = buttonColors[3];
+
+        buttonImage.color = normalColor;
+    }
+    protected override void OnHoverEntered(HoverEnterEventArgs args)
+    {
+        base.OnHoverEntered(args);
+        buttonImage.color = highlightedColor;
+        isPressed = false;
+    }
+    protected override void OnHoverExited(HoverExitEventArgs args)
+    {
+        base.OnHoverExited(args);
+        if (!isPressed)
+        {
+            buttonImage.color = normalColor;
+        }
+    }
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        isPressed = true;
+        buttonImage.color = pressedColor;
+    }
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        buttonImage.color = selectedColor;
+    }
+    void Update()
+    {
+        
+    }
+}
